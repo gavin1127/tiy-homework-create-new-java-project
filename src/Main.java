ba8a7a8a475cf7cfa93f95a4ba11471bc1b6472d
Gavin Shepherd <Gavinshepherd6688@gmail.com>
2017-03-03 01:12:04 -0500

    homework

---
+++ src/Main.java
@@ -0,0 +1,33 @@
+/**
+ * Created by Gavin on 3/3/17.
+ */
+public class Main{
+    public static void main(string[] args){
+        System.out.println(1+5);
+        //uses the addition operator to add the integers  1 to 5
+        System.out.println(1+5.5);
+        //uses the addition operator to add the integer 1 to the double 5.5
+        System.out.println(1-5);
+        //uses the subtraction operator to subtract he integers 1 to 5
+        System.out.println(1.0-5.5);
+        //uses the subtraction operator to subtract he doubles 1.0 to 5.5
+        System.out.println(1*5);
+        //uses the multiplication operator to multiply he integers 1 and 5
+        System.out.println(1/5);
+        //uses the division operator to divide he integers 1 from 5
+        System.out.println(15%2);
+        //uses the remainder operator to show the remainder of 15/2
+        System.out.println(15.0/2);
+        //uses the division operator to divide the double 15.0 from the integer 2
+        System.out.println(15.0/2.5);
+        //uses the division operator to divide the double 15.0 from the double 2.5
+        System.out.println((5+5)-5*3);
+        /** First 5+5 are added together because they are in parenthesis, then the 5*3 because its multiplication,
+        then the subtraction last */
+
+
+
+
+
+    }
+}
