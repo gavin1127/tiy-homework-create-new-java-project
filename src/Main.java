2983a0f2afc2e95d089bd0781f76863956d98c64
Gavin Shepherd <Gavinshepherd6688@gmail.com>
2017-03-06 21:33:36 -0500

    runs now

--- src/Main.java
+++ src/Main.java
@@ -2,11 +2,11 @@
  * Created by Gavin on 3/3/17.
  */
 public class Main{
-    public static void main(string[] args){
+    public static void main(String[] args){
         System.out.println(1+5);
         //uses the addition operator to add the integers  1 to 5
         System.out.println(1+5.5);
-        //uses the addition operator to add the integer 1 to the double 5.5
+        //uses the addition operator to add the integer 1 to the double 5.5, this will create a double answer.
         System.out.println(1-5);
         //uses the subtraction operator to subtract he integers 1 to 5
         System.out.println(1.0-5.5);
@@ -18,7 +18,7 @@ public class Main{
         System.out.println(15%2);
         //uses the remainder operator to show the remainder of 15/2
         System.out.println(15.0/2);
-        //uses the division operator to divide the double 15.0 from the integer 2
+        //uses the division operator to divide the double 15.0 from the integer 2, this will produce a double
         System.out.println(15.0/2.5);
         //uses the division operator to divide the double 15.0 from the double 2.5
         System.out.println((5+5)-5*3);
